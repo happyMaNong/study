11356b80ab47dd13405fe5af48b2e379594729c7
tianlingpeng <1194833510@qq.com>
2018-11-26 17:52:08 +0800

    添加实体类

---
+++ first-springboot/src/main/java/cn/tlp/springboot/entity/PlayerDTO.java
@@ -0,0 +1,16 @@
+package cn.tlp.springboot.entity;
+
+/**
+ * @author：tianlingpeng
+ * @crateDate：2018/11/26 17:49
+ */
+public class PlayerDTO {
+    private String gid;
+
+    private String name;
+
+    private int age;
+
+    private String nickname;
+
+}
